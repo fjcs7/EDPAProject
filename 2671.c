b1f27b09e9919304f1b47d82185f8bb956a85137
Fernando Junio Cunha e Sousa <fjcunha@ayesa.com>
2021-05-27 01:23:34 -0300

    - Criação da árvore concluida. - Percurso em largura ainda com erro.

--- 2671.c
+++ 2671.c
@@ -1,46 +1,152 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
+
+typedef struct reg {
+	char chave;
+	int nivel;
+	struct reg* esq;
+	struct reg* dir;
+} celula;
+
+typedef celula* arvore;
 
 #define true 1
 #define false 0
 #define SIZ 250
 
 int k;
-char ans[SIZ];
-char string[SIZ];
+int a;
+int tamanhoAtual;
+int proxFila;
+char saida[SIZ];
+char entrada[SIZ];
+int nivelMax;
+int qtNosUltimoNivel;
 
-void build(int, int);
+arvore cria_no(char, int);
+int calcula_altura_max(int);
+arvore construa_arvore(arvore, int);
+void percurso_em_largura(arvore);
 
 int main(int argc, char** argv)
 {
 
-    int n;
+	int n;
+	nivelMax = 0;
 
-    while (scanf("%d%*c", &n), n)
-    {
+	while (scanf("%d%*c", &n), n)
+	{
 
-        k = 0;
-        scanf("%[^\n]", string);
+		scanf("%[^\n]", entrada);
 
-        build(1, n);
-        ans[k] = 0;
-        printf("%s\n", ans);
+		k = 0;
+		nivelMax = calcula_altura_max(n);
+		qtNosUltimoNivel = numero_nos_folha(nivelMax, n);
+		arvore r = construa_arvore(NULL, 0);
 
-    }
+		percurso_em_largura(r);
 
-    return 0;
+		printf("%s\n", saida);
+
+	}
+
+	return 0;
 
 }
 
-void build(int u, int n)
-{
+int calcula_altura_max(int nrNos) {
+	return ceil(log2(nrNos));
+}
 
-    if (u > n)
-        return;
+int numero_nos_folha(int altura, int nrNos) {
 
-    build(2 * u, n);
-    ans[u - 1] = string[k++];
-    build(2 * u + 1, n);
+	int nivelAnterior = 0;
+	if (altura > 0) {
+		nivelAnterior = altura - 1;
+	}
 
+	return nrNos - (pow(2, altura) - 1);
+}
+
+arvore cria_no(char chave, int nivel) {
+	arvore no = (arvore)malloc(sizeof(celula));
+	no->chave = chave;
+	no->nivel = nivel;
+	no->esq = NULL;
+	no->dir = NULL;
+	return no;
+}
+
+arvore construa_arvore(arvore _no, int nivel) {
+
+	arvore no = _no;
+	if (nivel > nivelMax || nivel == nivelMax && qtNosUltimoNivel == 0) {
+		return NULL;
+	}
+
+	if (nivel == nivelMax) {
+		qtNosUltimoNivel--;
+	}
+
+	if (no == NULL) {
+		no = cria_no(NULL, nivel);
+	}
+
+	if (no->esq == NULL) {
+		no->esq = construa_arvore(no->esq, nivel + 1);
+	}
+
+	if (no->chave == NULL) {
+		no->chave = entrada[k++];
+	}
+
+	if (no->dir == NULL) {
+		no->dir = construa_arvore(no->dir, nivel + 1);
+	}
+
+	return no;
+}
+
+arvore iniciar_fila() {
+	return cria_no(0, -1);
+}
+
+arvore enfileirar(arvore fila, arvore no) {
+	if (no == NULL) {
+		return;
+	}
+	fila->dir = no;
+	return fila;
+}
+
+int fila_vazia(arvore fila) {
+	return fila->dir == NULL;
+}
+
+arvore desenfileirar(arvore fila) {
+	arvore no = fila->dir;
+	fila = no->dir;
+	return no;
+}
+
+void destruir_fila(arvore* fila) {
+	//free(fila);
+}
+
+void percurso_em_largura(arvore r) {
+	arvore f = iniciar_fila();
+	f = enfileirar(f, r);
+	a = -1;
+	
+	while (!fila_vazia(f)) {
+		r = desenfileirar(f);
+		if (r) {
+			f = enfileirar(f, r->esq);
+			f = enfileirar(f, r->dir);
+			saida[a++] = r->chave; /* visita raiz */
+		}
+	}
+	destruir_fila(&f);
 }
